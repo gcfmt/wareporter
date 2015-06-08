8aa596cdd4f0feeffe75205846ee17032d45d519
gcfmt <willyfestugatto@politec.mt.gov.br>
2015-06-08 17:47:31 -0400

    First

---
+++ WAReporter/JanelaAbrirArquivo.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WAReporter
+{
+    /// <summary>
+    /// Interaction logic for JanelaAbrirArquivo.xaml
+    /// </summary>
+    
+    public partial class JanelaAbrirArquivo : Window
+    {
+        public event EventHandler SelecaoOk;
+
+        public JanelaAbrirArquivo()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            arquivoButton.Focus();
+        }
+
+        private void arquivoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".db";
+            dlg.Filter = "Bancos de dados SQLite | *.db";
+
+            var result = dlg.ShowDialog();
+            
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                arquivoTextBox.Text = dlg.FileName;
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrWhiteSpace(arquivoTextBox.Text))
+            {
+                MessageBox.Show("É necessário preencher um nome de arquivo de banco de dados.");
+
+            }
+
+
+            if (File.Exists(arquivoTextBox.Text) && arquivoTextBox.Text.EndsWith("db"))
+            {
+                SelecaoOk(null, null);
+                this.Close();
+            } else
+                MessageBox.Show("Arquivo \"" + arquivoTextBox.Text + "\" não encontrado.");
+
+            arquivoButton.Focus();
+        }
+    }
+}
---
+++ WAReporter/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WAReporter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Data.SQLite.SQLiteConnection BancoAtual;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void AbrirCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var janelaAbrirArquivo = new JanelaAbrirArquivo();
+            janelaAbrirArquivo.SelecaoOk += delegate
+            {
+                BancoAtual = new SQLiteConnection("Data Source="+janelaAbrirArquivo.arquivoTextBox.Text+ ";Version=3;");
+                BancoAtual.Open();
+                var command = new SQLiteCommand("select * from chat_list;", BancoAtual);
+                var reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    var tx = reader.GetString(1);
+                }
+                BancoAtual.Close();
+
+            };
+            janelaAbrirArquivo.ShowDialog();
+        }
+
+        private void ExtrairCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SairCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
